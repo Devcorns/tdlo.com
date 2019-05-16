9966a5f7e8c81c271db2be2c6adfe1445942f677
Prakhar <prakhar@isglobalweb.com>
2019-05-16 18:16:20 +0530

    changes

--- tdlo/src/app/company/company-profile/company-profile.component.ts
+++ tdlo/src/app/company/company-profile/company-profile.component.ts
@@ -2,52 +2,42 @@ import { Component, OnInit, Input, ChangeDetectionStrategy,ChangeDetectorRef, Ng
 import { ActivatedRoute } from '@angular/router';
 import { GlobalService } from 'src/app/services/global.service';
 
-
-
 @Component({
   selector: 'app-company-profile',
   templateUrl: './company-profile.component.html',
   styleUrls: ['./company-profile.component.sass'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
+
 export class CompanyProfileComponent implements OnInit {
 
+  companyDetails = { 
+    name:"Company list will be visible here"
+  };
 
-   companyDetails = { name:"github"};
-
-  constructor(private route: ActivatedRoute,private gs:GlobalService,public cdr: ChangeDetectorRef) {
+  constructor(private route: ActivatedRoute, private gs: GlobalService, public cdr: ChangeDetectorRef) {
     
     this.route.url.subscribe(url => {
+
       const currentClassObj = this;
       let id: string = this.route.snapshot.params.id;
+      console.log("company-profile.component get id of company value", id);
 
-     console.log("company-profile.component get id of company value",id);
-     this.gs.get("api/user/get-company-details",id).subscribe(function(res:any) {
+      this.gs.get("api/user/get-company-details", id).subscribe(function(res: any) {
       
-      console.log("Get compnay details",res);
-     
-      currentClassObj.companyDetails = res;
-      currentClassObj.cdr.detectChanges();
+        console.log("Get compnay details", res);
+        currentClassObj.companyDetails = res;
+        currentClassObj.cdr.detectChanges();
+        console.log("call company details from company profile", currentClassObj, currentClassObj.companyDetails);
       
-      console.log("call company details from company profile",self, this.companyDetails);
-     // console.log(this.companyDetails)
-     
-      
+      });
+
     });
-
-        
-      
-    });
-
 
   }
 
-  ngOnInit() {
-    
-    
+  ngOnInit() { 
+
   }
-
-
-
 
 }

44ea8b6f2d3afb6e36cd50d6bf690cc8d049e262
Prakhar <prakhar@isglobalweb.com>
2019-06-05 15:59:26 +0530

    add company component resumes

--- tdlo/src/app/company/add-company/add-company.component.ts
+++ tdlo/src/app/company/add-company/add-company.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { GlobalService } from 'src/app/services/global.service';
+import { Config } from 'protractor';
 
 @Component({
   selector: 'app-add-company',
@@ -10,11 +12,31 @@ export class AddCompanyComponent implements OnInit {
 
   companyField = ['IT Industry', 'Accounting', 'C', 'D'];
   cmpnyDetails:FormGroup;
-  constructor(private fb: FormBuilder) { 
+  noMobile=false;
+  noPhone=false;
+
+
+  
+  constructor(private fb: FormBuilder, private gs: GlobalService) { 
+
     this.cmpnyDetails = this.fb.group({
-      companyName: new FormControl(''),
-      companyMobile: new FormControl(''),
-      companyAddr: new FormControl('')
+
+      companyName: new FormControl('',[Validators.required,Validators.minLength(2)]),
+
+      comapnyCategory:new FormControl('',[Validators.required]),
+
+      mobileNo: new FormControl('',[Validators.required,Validators.minLength(6)]),
+
+      phoneNo: new FormControl('',[Validators.required,Validators.minLength(6)]),
+
+      companyAddr: new FormControl('',[Validators.required,Validators.minLength(10)]),
+
+      countryCode: new FormControl('',[Validators.required,Validators.minLength(1)]),
+
+      stateCode :new FormControl('',[Validators.required,Validators.minLength(1)]),
+      
+      choosePhone:new FormControl('')
+
     })
   }
 
@@ -22,7 +44,25 @@ export class AddCompanyComponent implements OnInit {
   }
 
   sendDetails(cmpnyDetails) {
-    console.log(cmpnyDetails.value)
+    console.log(cmpnyDetails.value);
+    this.gs.post("api/user/addCompanyProfile",cmpnyDetails.value).subscribe((data:Config)=>{
+      console.log(data);
+    })
+
+  }
+
+  radioChange(data) {
+    console.log(data);
+    if( data==0 ){
+      this.noMobile = false;
+      this.noPhone = true;
+    } else if (data == 1) {
+      this.noMobile = true;
+      this.noPhone = false;
+    } else {
+      this.noPhone = true;
+      this.noMobile = true;
+    }
   }
 
 
